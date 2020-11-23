a4bc95deba182fb97a55c1af8d7e982c004daa65
Laurent Girod <laurent.girod@epfl.ch>
2020-11-23 15:57:18 +0100

    Constants for identifying data columns

---
+++ MCSAudit/constants.py
@@ -0,0 +1,18 @@
+"""
+Constants used within the program.
+"""
+
+KEY_LAT = "Latitude"
+KEY_LON = "Longitude"
+
+KEY_LAT_MEAN = "lat_avg"
+KEY_LON_MEAN = "lon_avg"
+
+KEY_CPM_MEAN = "cpm_avg"
+KEY_N = "n"
+KEY_VAL = "Value"
+
+KEY_MCC = "mcc"
+KEY_MNC = "mnc"
+KEY_LAC = "lac"
+KEY_CELL_ID = "Cellid"
